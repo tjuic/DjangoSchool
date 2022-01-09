35ac38ee112b9cbe422527c57808281a3e10a57e
tjunta <baymax11.10@gmail.com>
2022-01-09 16:58:02 -0600

    update

--- djangoschool/school/models.py
+++ djangoschool/school/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+class Score(models.Model):       #name as 'Score'
+
+    allSubject = (('Math', 'Math'),
+                  ('Science', 'Science'),
+                  ('Computer', 'Computer'),
+                  ('Physical Edu', 'Physical Edu'),
+                  ('Art', 'Art'),
+                  ('Physics', 'Physics'),
+                  ('Biology', 'Biology'),
+                  ('English', 'English'))
+
+    subject = models.CharField(max_length=200, choices=allSubject, default='Math')
+    student_name = models.CharField(max_length=200)     #what type of info, char info
+    score = models.IntegerField(default=0)
+
+    def __str__(self):      #__ dunder? waht to show for each sticky notes
+        return self.student_name + '-' + self.subject + '-' + str(self.score)
